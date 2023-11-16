aa2fde5770d1144c0523e9315a02101d53487350
Ketan Kadam <ketank39@gmail.com>
2023-11-16 09:56:15 +0530

    init

---
+++ android/app/src/main/kotlin/com/savings/ktn/savings_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.savings.ktn.savings_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,85 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import '../../../services/constants.dart';
+import '../../base/custom_image.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer.run(() {
+      Future.delayed(const Duration(seconds: 2), () {});
+      /*if (Get.find<AuthController>().isLoggedIn()) {
+        Get.find<AuthController>().getUserProfileData().then((value) {
+          Future.delayed(const Duration(seconds: 2), () {
+            if (Get.find<AuthController>().checkUserData()) {
+              Navigator.pushReplacement(
+                context,
+                getMaterialRoute(
+                  child: const Dashboard(),
+                ),
+              );
+            } else {
+              Navigator.pushReplacement(
+                context,
+                getMaterialRoute(
+                  child: const SignupScreen(),
+                ),
+              );
+            }
+          });
+        });
+      } else {
+        Navigator.pushReplacement(
+          context,
+          getMaterialRoute(
+            child: const LoginScreen(),
+          ),
+        );
+      }*/
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SizedBox(
+        width: size.width,
+        height: size.height,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Spacer(),
+            CustomImage(
+              path: Assets.imagesLogo,
+              height: size.height * .3,
+              width: size.height * .3,
+            ),
+            const Spacer(flex: 3),
+            Text(
+              AppConstants.appName,
+              style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                    fontSize: 26.0,
+                  ),
+            ),
+            Text(
+              "Subtitle",
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
